471700101688ec0922449917c9e949a4dbfa8d8f
ensar mustafa <ensarmustafa110@gmail.com>
2025-10-16 10:08:26 +0200

    p.p

--- personal.project/404.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<main>
-  <h1>Page Not Found</h1>
-  <p>Sorry, the page you are looking for does not exist.</p>
-  <a href="<?php echo home_url(); ?>">Return to Home</a>
-</main>
-<?php get_footer(); ?>
